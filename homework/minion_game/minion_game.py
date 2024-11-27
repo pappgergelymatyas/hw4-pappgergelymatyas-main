bccd2879d17d5ddd8976fbb88a618fdaf1a65db6
pappgergelymatyas <pappgergelymatyas@gmail.com>
2024-11-27 16:44:48 +0100

    mentés 2

--- homework/minion_game/minion_game.py
+++ homework/minion_game/minion_game.py
@@ -1,3 +1,21 @@
 def minion_game(s: str) -> str:
-    """Write your code here"""
+    vowels = "AEIOU"
+    stuart_score = 0
+    kevin_score = 0
+    n = len(s)
+
+    for i in range(n):
+        if s[i] in vowels:
+            kevin_score += n - i
+        else:
+            stuart_score += n - i
+
+    if stuart_score > kevin_score:
+        return f"Stuart {stuart_score}"
+    elif kevin_score > stuart_score:
+        return f"Kevin {kevin_score}"
+    else:
+        return "Draw"
     pass
+
+#juhu
